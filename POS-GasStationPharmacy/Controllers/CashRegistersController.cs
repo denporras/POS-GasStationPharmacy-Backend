7fb54b87ac56eb416a8e4399bb7fa8cf1d0f004e
Daedgomez <daedgomez@gmail.com>
2017-11-14 19:56:15 -0600

    [Add] Cash register

---
+++ POS-GasStationPharmacy/Controllers/CashRegistersController.cs
@@ -0,0 +1,47 @@
+﻿using POS_GasStationPharmacy.Models;
+using POS_GasStationPharmacy.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace POS_GasStationPharmacy.Controllers
+{
+    public class CashRegistersController : ApiController
+    {
+           CashRegistersRepository repository;
+           public CashRegistersController()
+        {
+            repository = new CashRegistersRepository();
+        }
+
+        public List<cash_register> Get()
+        {
+            return repository.GetAllCashRegisters();
+        }
+
+        public cash_register Get(int cash, int subsidiary, int employee, DateTime initial_time)
+        {
+            return repository.GetCashRegisterbyId(cash, subsidiary, employee, initial_time);
+        }
+
+        public void Post(cash_register c)
+        {
+            repository.AddCashRegister(c);
+        }
+
+
+        public void Put(cash_register c)
+        {
+            repository.UpdateCashRegister(c);
+        }
+        public void Delete(int cash, int subsidiary, int employee, DateTime initial_time)
+        {
+            repository.DeleteCashRegister(cash, subsidiary, employee, initial_time);
+        }
+ 
+    }
+}
---
+++ POS-GasStationPharmacy/Service/CashRegistersRepository.cs
@@ -0,0 +1,51 @@
+﻿using POS_GasStationPharmacy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace POS_GasStationPharmacy.Service
+{
+    public class CashRegistersRepository
+    {
+        PGSDbContext _context;
+        public CashRegistersRepository()
+        {
+            _context = new PGSDbContext();
+        }
+
+        public List<cash_register> GetAllCashRegisters()
+        {
+            var query = "SELECT * FROM getcashregisters();";
+            List<cash_register> pharma = _context.Database.SqlQuery<cash_register>(query).ToList();
+            return pharma;
+        }
+
+        public cash_register GetCashRegisterbyId(int cash, int subsidiary, int employee, DateTime initial_time)
+        {
+            var query = "SELECT * FROM getcashregister(" + cash + "," + subsidiary + ",'" + initial_time.Year + "-" + initial_time.Month + "-" + initial_time.Day + "'," + employee + ");";
+            cash_register pharma = _context.Database.SqlQuery<cash_register>(query).FirstOrDefault();
+            return pharma;
+        }
+
+        public void AddCashRegister(cash_register c)
+        {
+            var query = "SELECT addcashregister(" + c.cash + "," + c.subsidiary + ",'" + c.initial_time.Year + "-" + c.initial_time.Month + "-" + c.initial_time.Day + "'," + c.initial_cash.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture) + "," + c.employee + ");";
+            cash_register pharma = _context.Database.SqlQuery<cash_register>(query).FirstOrDefault();
+           
+        }
+
+        public void UpdateCashRegister(cash_register c)
+        {
+            var query = "SELECT updatecashregister(" + c.cash + "," + c.subsidiary + ",'" + c.initial_time.Year + "-" + c.initial_time.Month + "-" + c.initial_time.Day + "'," + c.employee + ",'" + c.final_time.Year + "-" + c.final_time.Month + "-" + c.final_time.Day + "'," + c.final_cash.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture) + ");";
+            cash_register pharma = _context.Database.SqlQuery<cash_register>(query).FirstOrDefault();
+
+        }
+
+        public void DeleteCashRegister(int cash, int subsidiary, int employee, DateTime initial_time)
+        {
+            var query = "SELECT deletecashregister(" + cash + "," + subsidiary + ",'" + initial_time.Year + "-" + initial_time.Month + "-" + initial_time.Day + "'," + employee + ");";
+            cash_register pharma = _context.Database.SqlQuery<cash_register>(query).FirstOrDefault();
+
+        }
+    }
+}
