a8ac40810e015050228dcf2c08f9d0f2bdf044b8
Gabriel <gbarboza963@outlook.com>
2017-11-14 21:21:18 -0600

    Reports

--- POS-GasStationPharmacy/Controllers/ReportsController.cs
+++ POS-GasStationPharmacy/Controllers/ReportsController.cs
@@ -1,24 +1,63 @@
-﻿using POS_GasStationPharmacy.Models;
+﻿using CrystalDecisions.CrystalReports.Engine;
+using POS_GasStationPharmacy.Models;
 using POS_GasStationPharmacy.Service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.IO;
+using CrystalDecisions.Shared;
+using System.Web;
 
 namespace POS_GasStationPharmacy.Controllers
 {
     public class ReportsController : ApiController
     {
-         ReportsRepository repository;
-         public ReportsController()
+        ReportsRepository repository;
+        PGSDbContext _context; // DataBase object
+        ReportDocument cryRpt; // crystal report object
+
+        public ReportsController()
         {
             repository = new ReportsRepository();
+            _context = new PGSDbContext();
+            cryRpt = new ReportDocument(); // instanciar crystal report 
         }
-        public report Get(report rep)
+        public HttpResponseMessage Get(report rep)
         {
-            return repository.GetReports(rep);
+            string fileName = "";
+            int flag = 1;
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            response.Content = new StringContent("The report isn't available");
+            if (rep.id == 3)
+            {
+                fileName = "LowInv";
+            }
+            else
+            {
+                flag = 0;
+            }
+
+            if (flag ==1)
+            {
+                CreatePDF(fileName + ".rpt");
+                response = new HttpResponseMessage(HttpStatusCode.OK);
+                response.Content = new StreamContent(new FileStream(HttpContext.Current.Server.MapPath("~/Reports/Reports_PDF/" + fileName + ".rpt"), FileMode.Open, FileAccess.Read));
+                response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+                response.Content.Headers.ContentDisposition.FileName = fileName + ".pdf";
+                response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/pdf");
+            }
+
+            return response;
+        }
+
+        // Function used to create the pdf of the crystal repor
+        private bool CreatePDF(string rptFileName)
+        {
+            cryRpt.Load(HttpContext.Current.Server.MapPath("~/Reports/" + rptFileName));
+            //CREATE THE PDF FILE USING CRYSTAL REPORT AND SAVE IN SERVER
+            cryRpt.ExportToDisk(ExportFormatType.PortableDocFormat, HttpContext.Current.Server.MapPath("~/Reports/Reports_PDF/" + rptFileName));
+            return true;
         }
     }
+        
 }
